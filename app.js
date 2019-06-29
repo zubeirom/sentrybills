3a3c2977ca8ac3bc10c5fc5720f8275933fd8f6c
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-06-30 00:44:33 +0200

    init

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// cors
+app.use(cors());
+
+// DB
+mongoose.connect('mongodb://heroku_3n6xpqgl:p1r4ivloddvbalov3eul6qf0q@ds243607.mlab.com:43607/heroku_3n6xpqgl' || 'mongodb://127.0.0.1:27017/myweightjourney', { useNewUrlParser: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+// Routes
+app.use('/', require('./routes/index'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
