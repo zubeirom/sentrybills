792874f04cc4dc822b8f391cd73ea212fdcaf7d9
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-07-09 16:10:38 +0200

    delete gcal in server

--- routes/bills.js
+++ routes/bills.js
@@ -4,9 +4,6 @@ const asyncHandler = require('express-async-handler');
 const JSONAPIDeserializer = require('jsonapi-serializer').Deserializer;
 const JSONAPISerializer = require('jsonapi-serializer').Serializer;
 const mongoose = require('mongoose');
-const fs = require('fs');
-const readline = require('readline');
-const { google } = require('googleapis');
 
 const router = express.Router();
 
@@ -19,7 +16,6 @@ const BillSerializer = new JSONAPISerializer('Bill', {
         'balanced',
         'addToCal',
         'userId',
-        'authUrl',
     ],
 });
 
@@ -110,126 +106,6 @@ router.post('/new', asyncHandler((req, res, next) => {
             const {
                 name, total, due, note, addToCal, balanced, userId,
             } = bill;
-
-            let authorize;
-            let getAccessToken;
-            let createEvent;
-
-            /* GOOGLE CALENDAR */
-            if (addToCal) {
-                // If modifying these scopes, delete token.json.
-                const SCOPES = ['https://www.googleapis.com/auth/calendar'];
-                // The file token.json stores the user's access and refresh tokens, and is
-                // created automatically when the authorization flow completes for the first
-                // time.
-                const TOKEN_PATH = 'token.json';
-
-                // Load client secrets from a local file.
-                fs.readFile('./credentials.json', (error, content) => {
-                    if (error) return console.log('Error loading client secret file:', error);
-                    // Authorize a client with credentials, then call the Google Calendar API.
-                    authorize(JSON.parse(content));
-                });
-
-                /**
-                * Create an OAuth2 client with the given credentials, and then execute the
-                * given callback function.
-                * @param {Object} credentials The authorization client credentials.
-                * @param {function} callback The callback to call with the authorized client.
-                */
-                authorize = (credentials, callback) => {
-                    // eslint-disable-next-line camelcase
-                    const { client_secret, client_id, redirect_uris } = credentials.web;
-                    const oAuth2Client = new google.auth.OAuth2(
-                        client_id, client_secret, redirect_uris[0],
-                    );
-
-                    // Check if we have previously stored a token.
-                    fs.readFile(TOKEN_PATH, (err1, token) => {
-                        if (err1) return getAccessToken(oAuth2Client, callback);
-                        oAuth2Client.setCredentials(JSON.parse(token));
-                        callback(oAuth2Client);
-                    });
-                };
-                /**
-                * Get and store new token after prompting for user authorization, and then
-                * execute the given callback with the authorized OAuth2 client.
-                * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
-                * @param {getEventsCallback} callback The callback for the authorized client.
-                */
-                getAccessToken = (oAuth2Client, callback) => {
-                    const authUrl = oAuth2Client.generateAuthUrl({
-                        access_type: 'offline',
-                        scope: SCOPES,
-                    });
-                    console.log('Authorize this app by visiting this url:', authUrl);
-                    const rl = readline.createInterface({
-                        input: process.stdin,
-                        output: process.stdout,
-                    });
-                    rl.question('Enter the code from that page here: ', (code) => {
-                        rl.close();
-                        oAuth2Client.getToken(code, (err2, token) => {
-                            if (err2) return console.error('Error retrieving access token', err);
-                            oAuth2Client.setCredentials(token);
-                            // Store the token to disk for later program executions
-                            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (e) => {
-                                if (e) return console.error(e);
-                                console.log('Token stored to', TOKEN_PATH);
-                            });
-                            callback(oAuth2Client);
-                        });
-                    });
-                };
-
-                // Refer to the Node.js quickstart on how to setup the environment:
-                // https://developers.google.com/calendar/quickstart/node
-                // Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
-                // stored credentials.
-
-                createEvent = (auth) => {
-                    const calendar = google.calendar({ version: 'v3', auth });
-                    const event = {
-                        summary: 'Google I/O 2015',
-                        location: '800 Howard St., San Francisco, CA 94103',
-                        description: 'A chance to hear more about Google\'s developer products.',
-                        start: {
-                            dateTime: '2019-07-10T09:00:00-07:00',
-                            timeZone: 'America/Los_Angeles',
-                        },
-                        end: {
-                            dateTime: '2015-05-28T17:00:00-07:00',
-                            timeZone: 'America/Los_Angeles',
-                        },
-                        recurrence: [
-                            'RRULE:FREQ=DAILY;COUNT=2',
-                        ],
-                        attendees: [
-                            { email: 'lpage@example.com' },
-                            { email: 'sbrin@example.com' },
-                        ],
-                        reminders: {
-                            useDefault: false,
-                            overrides: [
-                                { method: 'email', minutes: 24 * 60 },
-                                { method: 'popup', minutes: 10 },
-                            ],
-                        },
-                    };
-                    calendar.events.insert({
-                        auth,
-                        calendarId: 'primary',
-                        resource: event,
-                    }, (err3, eventlog) => {
-                        if (err) {
-                            console.log(`There was an error contacting the Calendar service: ${err3}`);
-                            return;
-                        }
-                        console.log('Event created: %s', eventlog.htmlLink);
-                    });
-                };
-            }
-            /* END */
 
             const newBill = new Bill({
                 name, total, due, note, addToCal, balanced, userId,
